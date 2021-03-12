9636bcf5f6a893535d81a5287e435647c4cafc3f
Denis Doroshenko <ddtlt1984@gmail.com>
2021-03-12 09:29:37 +0400

    Fixed

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Введите сумму платежа:");
-        int payment = scanner1.nextInt();
+        int payment = scanner.nextInt();
 
-        Scanner scanner2 = new Scanner(System.in);
         System.out.println("Введите сумму на балансе:");
-        int balance = scanner2.nextInt();
+        int balance = scanner.nextInt();
 
-        int bonus = payment >= 1099 ? payment/100 : 0;
+        int bonus = payment >= 1000 ? payment/100 : 0;
         balance += (payment + bonus);
 
         System.out.println("Спасибо за пополненние, Ваш баланс: " + balance + " руб. Ваш бонус составил: " + bonus + " руб.");
