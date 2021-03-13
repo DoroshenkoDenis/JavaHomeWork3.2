b6ae92fca2e3f64c3940493a3198016ab3e97184
Denis Doroshenko <ddtlt1984@gmail.com>
2021-03-13 20:25:34 +0400

    fixed

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите сумму платежа:");
-        int payment = scanner.nextInt();
-
+        double payment = scanner.nextDouble();
         System.out.println("Введите сумму на балансе:");
-        int balance = scanner.nextInt();
+        double balance = scanner.nextDouble();
+        int bonus = payment > 1000 ? (int) (payment / 100) : 0;
+        double totalBalance = (balance + payment + bonus);
 
-        int bonus = payment >= 1000 ? payment/100 : 0;
-        balance += (payment + bonus);
-
-        System.out.println("Спасибо за пополненние, Ваш баланс: " + balance + " руб. Ваш бонус составил: " + bonus + " руб.");
-
+        System.out.println("Спасибо за пополненние, Ваш баланс: " + totalBalance + " руб. Ваш бонус составил: " + bonus + " руб.");
     }
 }
